303493c67a9904e583272404a72c86532a831954
Haim Yadid <haim@nextinsurance.com>
2022-11-04 06:57:31 +0100

    move to junit jupyter fix compliation error

--- src/main/java/org/gckryptonites/StateWIthHighAllocation.java
+++ src/main/java/org/gckryptonites/StateWIthHighAllocation.java
@@ -2,6 +2,7 @@ package org.gckryptonites;
 
 import org.gckryptonites.anomalies.StateHolder;
 import org.gckryptonites.anomalies.BrutalAllocator;
+import org.gckryptonites.core.Mount;
 import org.gckryptonites.core.PauseDetector;
 
 /**
@@ -11,15 +12,14 @@ import org.gckryptonites.core.PauseDetector;
 public class StateWIthHighAllocation {
   public static void main(String[] args) throws InterruptedException {
     PauseDetector detector = new PauseDetector(500,10,100,System.err);
-    detector.start();
+    Mount.mount(detector);
     //Fragmenter f = new Fragmenter(2080,1024*1024,1024*1024,2,System.err);
     //f.start();
-    StateHolder bl = new StateHolder(10000,System.err);
-    bl.start();
-    BrutalAllocator b = new BrutalAllocator(10000,500,System.err);
-    b.start();
-    BrutalAllocator b2 = new BrutalAllocator(10000,500,System.err);
-    b2.start();
+    Mount.mount(new StateHolder(10000,System.err));
+
+    Mount.mount(new BrutalAllocator(10000,500,System.err));
+
+    Mount.mount(new BrutalAllocator(10000,500,System.err));
     while (true) {
       Thread.sleep(10000);
     }
